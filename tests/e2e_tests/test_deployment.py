28e4eea0ecf301a1d443f70d5b298b233e7b6d89
Eric Matthes <ehmatthes@gmail.com>
2024-12-13 10:10:34 -0500

    e2e test should work.

--- tests/e2e_tests/test_deployment.py
+++ tests/e2e_tests/test_deployment.py
@@ -23,49 +23,60 @@ def test_dummy(tmp_project, request):
 # Skip this test and enable test_dummy() to speed up testing of setup steps.
 # @pytest.mark.skip
 def test_deployment(tmp_project, cli_options, request):
-    """Test the full, live deployment process to Fly.io."""
+    """Test the full, live deployment process to {{PlatformName}}."""
 
     # Cache the platform name for teardown work.
-    request.config.cache.set("platform", "fly_io")
+    request.config.cache.set("platform", "{{PluginName}}")
 
-    print("\nTesting deployment to Fly.io using the following options:")
+    print("\nTesting deployment to {{PlatformName}} using the following options:")
     print(cli_options.__dict__)
 
     python_cmd = it_utils.get_python_exe(tmp_project)
 
+    # Note: If not using automate_all, take steps here that the end user would take.
     # Create a new project on the remote host, if not testing --automate-all.
-    if not cli_options.automate_all:
-        app_name = platform_utils.create_project()
-        request.config.cache.set("app_name", app_name)
+    # if not cli_options.automate_all:
+    #     app_name = platform_utils.create_project()
+    #     request.config.cache.set("app_name", app_name)
 
     # Run simple_deploy against the test project.
-    it_utils.run_simple_deploy(python_cmd, "fly_io", cli_options.automate_all)
+    it_utils.run_simple_deploy(python_cmd, automate_all=cli_options.automate_all)
 
     # If testing Pipenv, lock after adding new packages.
     if cli_options.pkg_manager == "pipenv":
         it_utils.make_sp_call(f"{python_cmd} -m pipenv lock")
 
+    # Note: This is an example of how you can stash information about the deployment, 
+    #   which can be used in the teardown phase. You do need to set project_url,
+    #   in order to run functionality tests against the deployed project.
+    # 
     # Get the deployed project's URL, and ID so we can destroy it later.
     #   This also commits configuration changes and pushes the project
     #   when testing the configuration-only workflow.
     # When testing automate_all, cache app_name for teardown work.
-    if cli_options.automate_all:
-        project_url, app_name = platform_utils.get_project_url_name()
-        request.config.cache.set("app_name", app_name)
-    else:
-        it_utils.commit_configuration_changes()
-        project_url = platform_utils.deploy_project(app_name)
+    # if cli_options.automate_all:
+    #     project_url, app_name = platform_utils.get_project_url_name()
+    #     request.config.cache.set("app_name", app_name)
+    # else:
+    #     it_utils.commit_configuration_changes()
+    #     project_url = platform_utils.deploy_project(app_name)
+    
+    # Note: ***** Remove this line, or your test will always report as passed! *****
+    remote_functionality_passed = True
 
-    # Remote functionality test fails if run too quickly after deployment.
+    # Remote functionality test often fails if run too quickly after deployment.
     print("\nPausing 10s to let deployment finish...")
     time.sleep(10)
 
+    # Note: Uncomment this section once your deployment is successful, and 
+    #   project_url is set in the above section.
+    # 
     # Test functionality of both deployed app, and local project.
     #   We want to make sure the deployment works, but also make sure we haven't
     #   affected functionality of the local project using the development server.
-    remote_functionality_passed = it_utils.check_deployed_app_functionality(
-        python_cmd, project_url
-    )
+    # remote_functionality_passed = it_utils.check_deployed_app_functionality(
+    #     python_cmd, project_url
+    # )
     local_functionality_passed = it_utils.check_local_app_functionality(python_cmd)
     log_check_passed = platform_utils.check_log(tmp_project)
 
--- tests/e2e_tests/utils.py
+++ tests/e2e_tests/utils.py
@@ -1,4 +1,7 @@
-"""Helper functions specific to Fly.io."""
+"""Helper functions specific to {{PlatformmName}}.
+
+Some Fly.io functions are included as an example.
+"""
 
 import re, time
 import json
@@ -6,66 +9,66 @@ import json
 from tests.e2e_tests.utils.it_helper_functions import make_sp_call
 
 
-def create_project():
-    """Create a project on Fly.io."""
-    print("\n\nCreating a project on Fly.io...")
-    output = (
-        make_sp_call(f"fly apps create --generate-name", capture_output=True)
-        .stdout.decode()
-        .strip()
-    )
-    print("create_project output:", output)
+# def create_project():
+#     """Create a project on Fly.io."""
+#     print("\n\nCreating a project on Fly.io...")
+#     output = (
+#         make_sp_call(f"fly apps create --generate-name", capture_output=True)
+#         .stdout.decode()
+#         .strip()
+#     )
+#     print("create_project output:", output)
 
-    re_app_name = r"New app created: (.*)"
-    app_name = re.search(re_app_name, output).group(1)
-    print(f"  App name: {app_name}")
+#     re_app_name = r"New app created: (.*)"
+#     app_name = re.search(re_app_name, output).group(1)
+#     print(f"  App name: {app_name}")
 
-    return app_name
-
-
-def deploy_project(app_name):
-    """Make a non-automated deployment."""
-    # Consider pausing before the deployment. Some platforms need a moment
-    #   for the newly-created resources to become fully available.
-    # time.sleep(30)
-
-    print("Deploying to Fly.io...")
-    make_sp_call("fly deploy")
-
-    # Open project and get URL.
-    output = (
-        make_sp_call(f"fly apps open -a {app_name}", capture_output=True)
-        .stdout.decode()
-        .strip()
-    )
-    print("fly open output:", output)
-
-    re_url = r"opening (http.*) \.\.\."
-    project_url = re.search(re_url, output).group(1)
-    if "https" not in project_url:
-        project_url = project_url.replace("http", "https")
-
-    print(f"  Project URL: {project_url}")
-
-    return project_url
+#     return app_name
 
 
-def get_project_url_name():
-    """Get project URL and app name of a deployed project.
-    This is used when testing the automate_all workflow.
-    """
-    output = (
-        make_sp_call("fly status --json", capture_output=True).stdout.decode().strip()
-    )
-    status_json = json.loads(output)
+# def deploy_project(app_name):
+#     """Make a non-automated deployment."""
+#     # Consider pausing before the deployment. Some platforms need a moment
+#     #   for the newly-created resources to become fully available.
+#     # time.sleep(30)
 
-    app_name = status_json["Name"]
-    project_url = f"https://{app_name}.fly.dev"
+#     print("Deploying to Fly.io...")
+#     make_sp_call("fly deploy")
 
-    print(f"  Found app name: {app_name}")
-    print(f"  Project URL: {project_url}")
+#     # Open project and get URL.
+#     output = (
+#         make_sp_call(f"fly apps open -a {app_name}", capture_output=True)
+#         .stdout.decode()
+#         .strip()
+#     )
+#     print("fly open output:", output)
 
-    return project_url, app_name
+#     re_url = r"opening (http.*) \.\.\."
+#     project_url = re.search(re_url, output).group(1)
+#     if "https" not in project_url:
+#         project_url = project_url.replace("http", "https")
+
+#     print(f"  Project URL: {project_url}")
+
+#     return project_url
+
+
+# def get_project_url_name():
+#     """Get project URL and app name of a deployed project.
+#     This is used when testing the automate_all workflow.
+#     """
+#     output = (
+#         make_sp_call("fly status --json", capture_output=True).stdout.decode().strip()
+#     )
+#     status_json = json.loads(output)
+
+#     app_name = status_json["Name"]
+#     project_url = f"https://{app_name}.fly.dev"
+
+#     print(f"  Found app name: {app_name}")
+#     print(f"  Project URL: {project_url}")
+
+#     return project_url, app_name
 
 
 def check_log(tmp_proj_dir):
@@ -88,17 +91,17 @@ def check_log(tmp_proj_dir):
     return True
 
 
-def destroy_project(request):
-    """Destroy the deployed project, and all remote resources."""
-    print("\nCleaning up:")
+# def destroy_project(request):
+#     """Destroy the deployed project, and all remote resources."""
+#     print("\nCleaning up:")
 
-    app_name = request.config.cache.get("app_name", None)
-    if not app_name:
-        print("  No app name found; can't destroy any remote resources.")
-        return None
+#     app_name = request.config.cache.get("app_name", None)
+#     if not app_name:
+#         print("  No app name found; can't destroy any remote resources.")
+#         return None
 
-    print("  Destroying Fly.io project...")
-    make_sp_call(f"fly apps destroy -y {app_name}")
+#     print("  Destroying Fly.io project...")
+#     make_sp_call(f"fly apps destroy -y {app_name}")
 
-    print("  Destroying Fly.io database...")
-    make_sp_call(f"fly apps destroy -y {app_name}-db")
+#     print("  Destroying Fly.io database...")
+#     make_sp_call(f"fly apps destroy -y {app_name}-db")
