217d1044d8b56da966ee3e422db0d9e4f5d03071
Eric Matthes <ehmatthes@gmail.com>
2025-05-25 10:27:11 -0400

    Test passes for NewFly plugin.

--- actual_tests/test_generator.py
+++ actual_tests/test_generator.py
@@ -2,6 +2,8 @@
 
 import argparse
 from argparse import Namespace
+from pathlib import Path
+from filecmp import dircmp
 
 import pytest
 
@@ -28,3 +30,36 @@ def test_no_spaces_anywhere(tmp_path_factory):
 
     args = Namespace(target_dir=tmp_path)
     gp.generate_plugin(plugin_config, args)
+
+    path_ref_dir = Path(__file__).parent / "reference_files" / "dsd-newfly"
+    path_test_plugin = tmp_path / "dsd-newfly"
+
+    # dc = dircmp(path_test_plugin, path_ref_dir, shallow=False, ignore=[".DS_Store"])
+    # assert not dc.left_only
+    # assert not dc.right_only
+    # assert not dc.diff_files
+    # assert not dc.funny_files
+    # breakpoint()
+    assert plugin_dirs_match(path_test_plugin, path_ref_dir)
+
+
+# --- Helper functions ---
+
+def plugin_dirs_match(test_dir, ref_dir):
+    """Check that a test plugin dir and a reference plugin dir match, recursively."""
+    dc = dircmp(test_dir, ref_dir, ignore=[".DS_Store", "__pycache__"], shallow=False)
+    # assert not dc.left_only
+    # assert not dc.right_only
+    # assert not dc.diff_files
+    # assert not dc.funny_files
+    check_dc(dc)
+    return True
+
+def check_dc(dc):
+    assert not dc.left_only
+    assert not dc.right_only
+    assert not dc.diff_files
+    assert not dc.funny_files
+    # breakpoint()
+    for dirname, dc in dc.subdirs.items():
+        check_dc(dc)
