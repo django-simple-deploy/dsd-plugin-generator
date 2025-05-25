e7343e0ad38469879c55e00e3ee03a74cca12d56
Eric Matthes <ehmatthes@gmail.com>
2025-05-25 12:56:47 -0400

    Started e2e test; skips if uv unavailable; builds new plugin in temp env.

---
+++ actual_tests/e2e_tests/test_basic_plugin.py
@@ -0,0 +1,50 @@
+"""Test a basic plugin in a real-world environment.
+
+This test:
+- Generates a new plugin.
+- Sets up a development environment for django-simple-deploy core.
+- Installs the new plugin to the development environment.
+- Runs the initial set of tests against the plugin.
+"""
+
+from argparse import Namespace
+from pathlib import Path
+import subprocess
+import shlex
+
+import pytest
+
+from utils.plugin_config import PluginConfig
+import generate_plugin as gp
+
+
+def uv_available():
+    """Ensure uv is available before running test."""
+    cmd = "uv self version -q"
+    cmd_parts = shlex.split(cmd)
+    try:
+        output = subprocess.run(cmd_parts, capture_output=True)
+        assert output.stdout.decode() == ""
+        return True
+    except FileNotFoundError:
+        # This is the exception raised on macOS when the command uv is unavailable.
+        return False
+
+
+### --- Test function ---
+
+@pytest.mark.skipif(not uv_available(), reason="uv must be installed in order to run e2e tests.")
+def test_new_plugin_e2e(tmp_path_factory):
+    tmp_path = tmp_path_factory.mktemp("e2e_new_plugin_test")
+    print(f"\nBuilding e2e test env at: {tmp_path.as_posix()}")
+
+    plugin_config = PluginConfig(
+        platform_name = "NewFly",
+        pkg_name = "dsd-newfly",
+        support_automate_all = True,
+        license_name = "eric",
+    )
+
+    args = Namespace(target_dir=tmp_path)
+    gp.generate_plugin(plugin_config, args)
+
