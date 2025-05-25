ce63320c9b8c58fd91e38c35ebaede47cd985284
Eric Matthes <ehmatthes@gmail.com>
2025-05-25 10:52:16 -0400

    Failing test for platform with space in name, also interpolation issue in deploy.py.

---
+++ actual_tests/integration_tests/reference_files/dsd-newfly-single-space/README.md
@@ -0,0 +1,3 @@
+# dsd-newfly
+
+A plugin for deploying Django projects to New Fly, using django-simple-deploy.
--- actual_tests/integration_tests/test_generator.py
+++ actual_tests/integration_tests/test_generator.py
@@ -31,6 +31,26 @@ def test_no_spaces_anywhere(tmp_path_factory):
     dc = dircmp(path_test_plugin, path_ref_dir, ignore=[".DS_Store", "__pycache__"])
     assert_dirs_match(dc)
 
+def test_single_space_platform_name(tmp_path_factory):
+    tmp_path = tmp_path_factory.mktemp("sample_plugin")
+    print(f"\nWriting plugin to: {tmp_path.as_posix()}")
+
+    plugin_config = PluginConfig(
+        platform_name = "New Fly",
+        pkg_name = "dsd-newfly",
+        support_automate_all = True,
+        license_name = "eric",
+    )
+
+    args = Namespace(target_dir=tmp_path)
+    gp.generate_plugin(plugin_config, args)
+
+    path_ref_dir = Path(__file__).parent / "reference_files" / "dsd-newfly-single-space"
+    path_test_plugin = tmp_path / "dsd-newfly"
+
+    dc = dircmp(path_test_plugin, path_ref_dir, ignore=[".DS_Store", "__pycache__"])
+    assert_dirs_match(dc)
+
 
 # --- Helper functions ---
 
