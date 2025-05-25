c0ab0526c9e4617eed5347ece5679a2847d7cb1d
Eric Matthes <ehmatthes@gmail.com>
2025-05-25 10:42:40 -0400

    Remove unused code, better names.

--- actual_tests/integration_tests/test_generator.py
+++ actual_tests/integration_tests/test_generator.py
@@ -11,11 +11,6 @@ from utils.plugin_config import PluginConfig
 import generate_plugin as gp
 
 
-# @pytest.fixture(tmp_dir):
-# def get_tmp_dir():
-#     tmp_path = tmp_path_factory.mktemp("sample_code")
-#     print(f"\nWriting plugin to: {tmp_path.as_posix()}")
-
 def test_no_spaces_anywhere(tmp_path_factory):
     tmp_path = tmp_path_factory.mktemp("sample_plugin")
     print(f"\nWriting plugin to: {tmp_path.as_posix()}")
@@ -33,22 +28,18 @@ def test_no_spaces_anywhere(tmp_path_factory):
     path_ref_dir = Path(__file__).parent / "reference_files" / "dsd-newfly"
     path_test_plugin = tmp_path / "dsd-newfly"
 
-    assert plugin_dirs_match(path_test_plugin, path_ref_dir)
+    dc = dircmp(path_test_plugin, path_ref_dir, ignore=[".DS_Store", "__pycache__"])
+    assert_dirs_match(dc)
 
 
 # --- Helper functions ---
 
-def plugin_dirs_match(test_dir, ref_dir):
-    """Check that a test plugin dir and a reference plugin dir match, recursively."""
-    dc = dircmp(test_dir, ref_dir, ignore=[".DS_Store", "__pycache__"], shallow=False)
-    check_dc(dc)
-    return True
-
-def check_dc(dc):
+def assert_dirs_match(dc):
+    """Check there are no differences in the dircmp object, and recurse all subdirs."""
     assert not dc.left_only
     assert not dc.right_only
     assert not dc.diff_files
     assert not dc.funny_files
 
     for dirname, dc in dc.subdirs.items():
-        check_dc(dc)
+        assert_dirs_match(dc)
