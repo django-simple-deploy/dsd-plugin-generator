9c738ebd63c8f50f4e68aa5bab746d9fa2fa1153
Eric Matthes <ehmatthes@gmail.com>
2025-05-26 08:10:49 -0400

    Remove unused code.

--- generate_plugin.py
+++ generate_plugin.py
@@ -202,13 +202,6 @@ def generate_plugin(plugin_config, args):
         new_contents = "\n".join(new_lines)
         path.write_text(new_contents)
 
-    # # Remove unneeded lines from README.
-    # print("Modifying README...")
-    # path = path_root_new / "README.md"
-    # lines = path.read_text().splitlines()[:4]
-    # contents = "\n".join(lines)
-    # path.write_text(contents)
-
     msg = "\nFinished setting up your plugin. If there are any issues,"
     msg += "\nplease delete the new plugin and try again, or make manual changes"
     msg += "\nand file an issue on this project's repo:"
