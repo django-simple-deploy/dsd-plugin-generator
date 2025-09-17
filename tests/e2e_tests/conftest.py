d90b587143bb6d39f480d67ff3a38d7482a42d19
Eric Matthes <ehmatthes@gmail.com>
2025-09-17 12:57:58 -0400

    --include-core-tests -> --run-core-tests

--- tests/e2e_tests/conftest.py
+++ tests/e2e_tests/conftest.py
@@ -7,17 +7,17 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--include-core-tests",
+        "--run-core-tests",
         action="store_true",
         help="Run the full set of core django-simple-deploy tests for each new plugin.",
     )
 
 @dataclass
 class CLIOptions:
-    include_core_tests: bool=False
+    run_core_tests: bool=False
 
 @pytest.fixture(scope="session")
 def cli_options(request):
     return CLIOptions(
-        include_core_tests=request.config.getoption("--include-core-tests"),
+        run_core_tests=request.config.getoption("--run-core-tests"),
     )
--- tests/e2e_tests/utils/e2e_utils.py
+++ tests/e2e_tests/utils/e2e_utils.py
@@ -47,7 +47,7 @@ def get_core_plugin_test_cmd(path_dsd, cli_options, platform_name_lower):
     """Get the command for running django-simple-deploy tests after a new plugin has been installed."""
     test_filename = f"test_{platform_name_lower}_config.py"
 
-    if cli_options.include_core_tests:
+    if cli_options.run_core_tests:
         # Run full set of core django-simple-deploy tests, and plugin integration tests.
         cmd = f"cd {path_dsd.as_posix()} && source .venv/bin/activate && pytest"
     else:
@@ -69,7 +69,7 @@ def check_core_plugin_tests(stdout, cli_options, core_only=False):
         if core_only:
             # Core tests, no plugin installed.
             assert passed >= 31
-        elif cli_options.include_core_tests:
+        elif cli_options.run_core_tests:
             # Core tests, and plugin's integrationtests.
             assert passed >= 65
         else:
